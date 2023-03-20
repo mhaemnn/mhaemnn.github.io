78128705c819b9199d794185b6cbee424025e020
mhaemnn <ismuhaimin1@gmail.com>
2023-03-20 22:24:30 +0700

    edit code

--- src/js/scrollreveal.js
+++ src/js/scrollreveal.js
@@ -6,3 +6,8 @@ const sr = ScrollReveal({
 });
 
 sr.reveal("header", { origin: "top", delay: "200" });
+sr.reveal("section", { origin: "top", delay: 200 });
+sr.reveal(".about", { origin: "top", delay: "200" });
+sr.reveal(".education", { origin: "left", delay: 200 });
+sr.reveal(".experience", { origin: "right", delay: 200 });
+sr.reveal(".card", { origin: "left", delay: 300 });
